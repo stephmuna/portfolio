3fbe254027964d294964690e8e46341519b20659
stephmuna <mndubisi@ucsd.edu>
2025-10-17 02:10:04 -0700

    JS added

---
+++ global.js
@@ -0,0 +1,105 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/stephmuna', title: 'GitHub' }
+  ];
+
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+  const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                  // Local server
+    : "/portfolio/";         // GitHub Pages repo name
+  
+  for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+  
+    
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+  
+    // Create link and add it to nav
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+  }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+              <option value="light dark">Automatic</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+          </select>
+      </label>`,
+  );
+
+  const select = document.querySelector('.color-scheme select')
+
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
+  });
+
+
+  if ('colorScheme' in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+  }
+
+
+  
+const form = document.querySelector('form[action^="mailto:"]');
+
+form?.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+
+  const data = new FormData(form);
+
+  const params = [];
+  for (let [name, value] of data) {
+    
+    params.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+  }
+
+  const sep = form.action.includes('?') ? '&' : '?';
+  const url = form.action + (params.length ? sep + params.join('&') : '');
+
+  location.href = url; 
+});
+
+
+
+  
+
+  
+  
+
+
+
+
+
+  
